77cf9c116e3babfba783a2e2e303b498f3de00e2
flight <mwxj@mail.ru>
2022-05-03 18:54:00 +0300

    initial commit

---
+++ src/main/java/flight/overall/OverallApplication.java
@@ -0,0 +1,13 @@
+package flight.overall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverallApplication.class, args);
+    }
+
+}
