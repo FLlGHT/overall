e1ef63437d8500b496d408e173fcffd61c8287be
fedorenko <fedorenko@reshenie-soft.ru>
2022-05-04 13:20:56 +0300

    # Thymeleaf bug fix

--- src/main/java/flight/overall/controller/UserController.java
+++ src/main/java/flight/overall/controller/UserController.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -23,9 +22,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
     @GetMapping("/{username}")
-    public String getUserInfo(@PathVariable String username,
-                                    Model model) {
+    public String getUserInfo(@PathVariable String username, Model model) {
 
         Optional<UserData> userData = userService.getUserData(username);
         userData.ifPresent(data -> userService.addAttributesToUser(data, model));
