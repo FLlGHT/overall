0460d6ad1b4338d002ea5d34c3bbfd2a2aed6eea
fedorenko <fedorenko@reshenie-soft.ru>
2022-05-05 10:36:17 +0300

    # clean import

--- src/main/java/flight/overall/controller/UserController.java
+++ src/main/java/flight/overall/controller/UserController.java
@@ -1,16 +1,13 @@
 package flight.overall.controller;
 
-import flight.overall.entity.Post;
 import flight.overall.entity.UserData;
 import flight.overall.service.PostService;
 import flight.overall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
