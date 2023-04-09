9644b0b106410b561b67029a2088871edfcef6c7
flight <mwxj@mail.ru>
2023-04-09 19:17:55 +0300

    Fix StringUtils

---
+++ overall/src/main/java/com/flight/overall/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.flight.overall.utils;
+
+/**
+ * @author FLIGHT
+ * @date 09.04.2023
+ */
+public class StringUtils {
+
+
+  public static boolean isNullOrEmpty(String string) {
+    return string == null || string.isEmpty();
+  }
+}
